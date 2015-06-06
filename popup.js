523023c76c802994b81f42749ccac7b4d08fb7e8
tylerrinnan <tyler.rinnan@gmail.com>
2015-06-06 11:37:23 -0500

    Additional Removals
    local variables cannot be used to store left and right positions since
    each plugin instance on a window generates and overwrites global
    variables. Chrome storage will need to be used to retrieve settings for
    orientation and affiliated id before updating. More to come in future
    checkins...

--- popup.js
+++ popup.js
@@ -1,124 +1,112 @@
+var left = 'left';
+var right = 'right';
+var leftId;
+var rightId;
+var maxWidth = 9999; // chrome will resize to its maximum limit
+var minWidth = 0; // chrome will resize to its minimum limit
 
+var resize = function (window) {
+	if(!leftId || !rightId){
+		console.log('you dont have any windows designated');
+		return;
+	}
 
-function renderSelectors(statusText) {
-	document.getElementById('selector').textContent = statusText;
-}
-
-var currTabs;
-var currWindow;
-var maxWidth = 9999; // chrome will resize to its maximum limit, therefore 9999 is fine
-var minWidth = 0; // chrome will resize to its minimum limit, therefore 0 is fine
-var counter = 0;
-
-// var resize = function (window, resizeBy) {
-// 	if (counter < stop) {
-// 		counter++;
-// 		chrome.windows.update(window.id, {
-// 			width : window.width + resizeBy,
-// 			height : window.height
-// 		}, function (win) {
-// 			resize(win, sizeToResizeBy);
-// 		});
-// 		//console.log(counter);
-// 	} else {
-// 		//console.log('made it out of the loop');
-// 	}
-// };
-
-var resize = function (window, resizeBy, minOrMax) {
-		var width = minOrMax === 0 ? minWidth : maxWidth;
+	if(window.id === leftId){
 		chrome.windows.update(window.id, {
-			width : width,
+			width : 900,
 			height : window.height
-		}, function (win) {
-			//do nothing here.
-		})
+		});
+	}else if(window.id === rightId){
+		console.log('would have resized right side');
+	}
+
 };
 
 var bindEventHandlers = function () {
 	$('#orientation button').bind('click', function (event) {
 		designateOrienation(event);
-	});
-
-	$('#update').on('click', {value: this}, function (event) {
-		updateResizeHandler();
-	});
-};
-
-var updateResizeHandler = function () {
-	var $resizeInput = $('#resizeInput');
-
-	if($resizeInput === []){
-		console.log('updateResizeHandler, update textbox not found');
-		return;
-	}
-
-	//Grab the value to store, and clear out the textbox
-	//TODO: Move this out into it's own function
-	var val = $resizeInput.val();
-	$resizeInput.val('');
-
-	if(val){
-		saveToStorage()
-	};
+	})
 };
 
 var designateOrienation = function (event) {
 		chrome.windows.getCurrent(function (win) {
-			var json = {};
-			json['pr_' + win.id] = {
-				"orientation" : event.toElement.dataset.orientation,
-			};
-
-			saveToStorage(json, function () {
-				//do nothing with callback.
-				//there is a listener bound to storage that will notify the user when storage has been modified.
-			});
+			var orientation = event.toElement.dataset.orientation
+			if(orientation == left){
+				leftId = win.id;
+			}else if(orientation == right){
+				rightId = win.id;
+			}else{
+				//tampered with data values
+				console.log('messing with data values will cause CWM to work incorrectly...');
+				return;
+			}
 		});
 };
 
-// Takes an object in the following format {'key': 'value'}
-var saveToStorage = function(obj, callback){
-	var storage = getChromeStorage();
-	if(obj){
-		storage.set(obj, callback);
-	}else{
-		console.log('saveToStorage, falsy parm passed: ' + obj);
-	}
-}
-
-var getFromStorage = function(key){
-	var storage = getChromeStorage();
-	if(!key || key.length <= 0){
-		console.log('getFromStorage, invalid key: ' + key);
-		return;
-	}
-
-	storage.get(key, function(obj){
-		console.log(obj["pr_1"].orientation);
-	});
-}
-
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-	 for (key in changes) {
-		 var storageChange = changes[key];
-		 console.log('Storage key "%s" in namespace "%s" changed. ' +
-			 'Old value was "%s", new value is "%s".',
-			 key,
-			 namespace,
-			 storageChange.oldValue,
-			 storageChange.newValue);
-	 }
-});
-
-var getChromeStorage = function(){
-	if(chrome && chrome.storage && chrome.storage.sync){
-		return chrome.storage.sync;
-	}else{
-		console.log('getChromeStorage, chrome storage not available. Check manifest.json');
-	}
-}
-
+//bind event listeners:
 document.addEventListener('DOMContentLoaded', function () {
 	bindEventHandlers();
 });
+
+chrome.windows.onFocusChanged.addListener(function(windowId){
+	if(windowId == chrome.windows.WINDOW_ID_NONE){
+		//do nothing, focus has lost all chrome windows
+		return;
+	};
+
+	resize(windowId);
+});
+
+// var json = {};
+// json['pr_' + win.id] = {
+// 	"orientation" : event.toElement.dataset.orientation,
+// };
+//
+// saveToStorage(json, function () {
+// 	//do nothing with callback.
+// 	//there is a listener bound to storage that will notify the user when storage has been modified.
+// });
+
+// // Takes an object in the following format {'key': 'value'}
+// var saveToStorage = function(obj, callback){
+// 	var storage = getChromeStorage();
+// 	if(obj){
+// 		storage.set(obj, callback);
+// 	}else{
+// 		console.log('saveToStorage, falsy parm passed: ' + obj);
+// 	}
+// }
+//
+// var getFromStorage = function(key){
+// 	var storage = getChromeStorage();
+// 	if(!key || key.length <= 0){
+// 		console.log('getFromStorage, invalid key: ' + key);
+// 		return;
+// 	}
+//
+// 	storage.get(key, function(obj){
+// 		console.log(obj["pr_1"].orientation);
+// 	});
+// };
+//
+// var getChromeStorage = function(){
+// 	if(chrome && chrome.storage && chrome.storage.sync){
+// 		return chrome.storage.sync;
+// 	}else{
+// 		console.log('getChromeStorage, chrome storage not available. Check manifest.json');
+// 	}
+// }
+
+
+
+// chrome.storage.onChanged.addListener(function (changes, namespace) {
+// 	 for (key in changes) {
+// 		 var storageChange = changes[key];
+// 		 console.log('Storage key "%s" in namespace "%s" changed. ' +
+// 			 'Old value was "%s", new value is "%s".',
+// 			 key,
+// 			 namespace,
+// 			 storageChange.oldValue,
+// 			 storageChange.newValue);
+// 	 };
+// }
